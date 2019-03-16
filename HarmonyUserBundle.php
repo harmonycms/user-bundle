ad27a8b55f9701996064491b5833ae71e7e8f928
David Sanchez <david38sanchez@gmail.com>
2019-03-16 18:40:59 -0400

    Register DoctrineMongoDBMappingsPass files

--- HarmonyUserBundle.php
+++ HarmonyUserBundle.php
@@ -3,6 +3,7 @@
 namespace Harmony\Bundle\UserBundle;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
 use Harmony\Bundle\UserBundle\DependencyInjection\Compiler\ValidationPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -26,8 +27,12 @@ class HarmonyUserBundle extends Bundle
         $container->addCompilerPass(new ValidationPass());
 
         $mappings = [realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'Harmony\Bundle\UserBundle\Model'];
-        if (class_exists(DoctrineOrmMappingsPass::class) && $container->has('doctrine.orm.default_entity_manager')) {
+        if (\class_exists(DoctrineOrmMappingsPass::class) && $container->has('doctrine.orm.default_entity_manager')) {
             $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
+        }
+
+        if (\class_exists(DoctrineMongoDBMappingsPass::class)) {
+            $container->addCompilerPass(DoctrineMongoDBMappingsPass::createXmlMappingDriver($mappings, []));
         }
     }
 }
