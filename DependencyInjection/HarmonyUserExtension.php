393ea43591eeef4675994694d786831cb7f65d0d
David Sanchez <david.sanchez@nventive.com>
2019-03-27 16:38:50 -0400

    Add admin model configuration

--- DependencyInjection/HarmonyUserExtension.php
+++ DependencyInjection/HarmonyUserExtension.php
@@ -4,15 +4,18 @@ namespace Harmony\Bundle\UserBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use function array_filter;
+use function array_shift;
 
 /**
  * Class HarmonyUserExtension
  *
  * @package Harmony\Bundle\UserBundle\DependencyInjection
  */
-class HarmonyUserExtension extends Extension
+class HarmonyUserExtension extends Extension implements PrependExtensionInterface
 {
 
     /**
@@ -35,5 +38,24 @@ class HarmonyUserExtension extends Extension
         $container->setParameter('harmony_user.password_reset.email_from',
             $config['password_reset']['email_from'] ?? null);
         $container->setParameter('harmony_user.password_reset.token_ttl', $config['password_reset']['token_ttl']);
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        // get all bundles
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['HarmonyAdminBundle'])) {
+            $loader->load('admin.yaml');
+            $config = array_filter($container->getExtensionConfig('harmony_admin'));
+            $container->setParameter('harmony_admin.config', ['harmony_admin' => array_shift($config)]);
+        }
     }
 }
