0913eef77b755296e73fa8fc71d40ddfad16701b
David Sanchez <david38sanchez@gmail.com>
2019-04-11 00:05:04 -0400

    Fixing code

--- Form/Type/UserRoleType.php
+++ Form/Type/UserRoleType.php
@@ -5,7 +5,6 @@ namespace Harmony\Bundle\UserBundle\Form\Type;
 use Harmony\Bundle\UserBundle\Security\UserInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
